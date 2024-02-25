04481c303ee2a550e9df07e56169279f3f92d8b0
Evgenii <markine087@gmail.com>
2024-02-25 16:27:59 +0300

    some fix

--- orderServiceMicroservice/src/main/java/com/example/orderservicemicroservice/services/OrderService.java
+++ orderServiceMicroservice/src/main/java/com/example/orderservicemicroservice/services/OrderService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
