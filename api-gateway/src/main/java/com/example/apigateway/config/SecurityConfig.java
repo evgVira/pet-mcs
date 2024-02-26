48aa12a8ccfdce5539e81c0582e65db908dbdd5b
Evgenii <markine087@gmail.com>
2024-02-26 17:31:17 +0300

    add keycloack

---
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity)throws Exception{
+        httpSecurity
+                .csrf(csrfSpec -> csrfSpec.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
+    }
+}
---
+++ discovery-server/src/main/java/com/example/discoveryserver/configurations/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.discoveryserver.configurations;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration  {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/eureka/**"));
+        return httpSecurity.build();
+    }
+
+
+
+
+
+}
